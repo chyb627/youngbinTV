92b8ed40a51381d245111f3c4c3d4dd8286fdbe2
cha <chyb627@naver.com>
2023-02-13 02:37:24 +0900

    Iframe

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,5 +1,7 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import styled from 'styled-components';
+
 import { movieDetail } from '../actions/movie';
 import { AppDispatch } from '../store';
 import { RootState } from '../store/reducer';
@@ -8,7 +10,9 @@ import '../css/Banner.css';
 const Banner = () => {
   const dispatch = useDispatch<AppDispatch>();
   const { movieDetailData: movie } = useSelector((state: RootState) => state.movie);
-  // console.log(':::movieDetailData::', movieDetailData);
+  // console.log(':::movie::', movie);
+
+  const [isClicked, setIsClicked] = useState(false);
 
   useEffect(() => {
     dispatch(movieDetail());
@@ -19,27 +23,83 @@ const Banner = () => {
     return str?.length > n ? str.substring(0, n) + '...' : str;
   }, []);
 
-  return (
-    <header
-      className="banner"
-      style={{
-        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
-        backgroundPosition: 'top center',
-        backgroundSize: 'cover',
-      }}
-    >
-      <div className="banner__contents">
-        <h1 className="banner_title">{movie.title || movie.name || movie.original_name}</h1>
+  // 화면 분기 처리 (클릭시 iframe화면)
+  if (isClicked) {
+    return (
+      <>
+        <button onClick={() => setIsClicked(false)}>뒤로가기</button>
 
-        <div className="banner_buttons">
-          {movie?.videos?.results[0]?.key && <button className="banner__button play">Play</button>}
+        <Container>
+          <HomeContainer>
+            <Iframe
+              width="640"
+              height="360"
+              src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?controls=0&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0].key}`}
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            />
+          </HomeContainer>
+        </Container>
+      </>
+    );
+  } else {
+    return (
+      <header
+        className="banner"
+        style={{
+          backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+          backgroundPosition: 'top center',
+          backgroundSize: 'cover',
+        }}
+      >
+        <div className="banner__contents">
+          <h1 className="banner_title">{movie.title || movie.name || movie.original_name}</h1>
+
+          <div className="banner_buttons">
+            {movie?.videos?.results[0]?.key && (
+              <button onClick={() => setIsClicked(true)} className="banner__button play">
+                Play
+              </button>
+            )}
+          </div>
+
+          <p className="banner__description">{truncate(movie.overview, 100)}</p>
         </div>
-
-        <p className="banner__description">{truncate(movie.overview, 100)}</p>
-      </div>
-      <div className="banner--fadeBottom" />
-    </header>
-  );
+        <div className="banner--fadeBottom" />
+      </header>
+    );
+  }
 };
 
 export default Banner;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+`;
+
+const HomeContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const Iframe = styled.iframe`
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  opacity: 0.65;
+  border: none;
+
+  &::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+`;
